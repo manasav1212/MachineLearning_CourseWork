423e6a9f78ea394e3d47258cb4c723c7cfbf06fa
manasav1212 <manasav@unm.edu>
2026-04-02 16:54:59 -0600

    Assignment 3

---
+++ assignment_3/src/dataPreperation.py
@@ -0,0 +1,4 @@
+# You are required to download and transform the text reviews to term frequency-
+# inverse document frequency (tf-idf) vectors. You may use the approach and the programs in the textbook
+# to do so. Make sure that all text reviews are cleaned and correctly transformed. You may use 70% data as
+# training data and the rest as test data.
